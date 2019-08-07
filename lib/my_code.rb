77b6f3078c85259560cd62ecd9baefc9770b6070
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-07 18:59:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,13 @@
 # My Code here....
 
 map_to_negativize(source_array)
+ new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 )
+    i += 1
+  end
+  return new
+end
+
 
