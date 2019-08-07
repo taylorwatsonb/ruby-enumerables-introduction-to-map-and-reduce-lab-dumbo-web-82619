91d7d1701f2b85c4e3b2ffe4774c7189750fb739
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-07 19:03:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,13 +16,16 @@ new = []
 i = 0 
 
 while i < source_array.length do
-  new.push( source_array[i] * 1)
+  new.push( source_array)
    i += 1
   end
   return new 
 end 
   
-  
+ map_to_double(source_array)
+ 
+ new = []
+ i = 0
   
   
   
